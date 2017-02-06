fbac876e11337ebd8a444fcc13669ef7fb66fb01
Clinton Andrews <candrews800@gmail.com>
2017-02-05 21:50:22 -0500

    Add menu structure

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -7,7 +7,7 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     $scope.authentication = Authentication;
 
     // Get the topbar menu
-    $scope.menu = Menus.getMenu('topbar');
+    $scope.menu = document.getElementById("side-menu");
 
     // Toggle the menu items
     $scope.isCollapsed = false;
@@ -19,5 +19,17 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     $scope.$on('$stateChangeSuccess', function () {
       $scope.isCollapsed = false;
     });
+
+    $scope.toggleMenu = function() {
+        if ($scope.menu.style.left === "-200px") {
+            $scope.menu.style.left = "0px";
+        } else {
+            $scope.menu.style.left = "-200px";
+        }
+    };
+
+    $scope.closeMenu = function() {
+      $scope.menu.style.left = "-200px";
+    }
   }
 ]);
