c214ce14297a000f7d12a9f38bf17ec3a65b7dd3
Clinton Andrews <candrews800@gmail.com>
2017-02-07 23:26:10 -0500

    Change code in correct location

--- modules/articles/server/routes/articles.server.routes.js
+++ modules/articles/server/routes/articles.server.routes.js
@@ -3,8 +3,21 @@
 /**
  * Module dependencies.
  */
-var projects = require('../controllers/projects.server.controller');
+var articlesPolicy = require('../policies/articles.server.policy'),
+  articles = require('../controllers/articles.server.controller');
 
 module.exports = function (app) {
-  // Reference modules/articles/server for examples on what can go here
+  // Articles collection routes
+  app.route('/api/articles').all(articlesPolicy.isAllowed)
+    .get(articles.list)
+    .post(articles.create);
+
+  // Single article routes
+  app.route('/api/articles/:articleId').all(articlesPolicy.isAllowed)
+    .get(articles.read)
+    .put(articles.update)
+    .delete(articles.delete);
+
+  // Finish by binding the article middleware
+  app.param('articleId', articles.articleByID);
 };
