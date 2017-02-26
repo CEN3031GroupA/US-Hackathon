7bc8840923ab16329671abadfead6bfd33cf4fd3
hackleman <hackleman.jason@gmail.com>
2017-02-25 23:08:51 -0600

    and now the other folder

--- modules/ideas/server/models/ideas.server.model.js
+++ modules/ideas/server/models/ideas.server.model.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use strict';
 
 var mongoose = require('mongoose'),
@@ -52,58 +52,3 @@ var IdeaSchema = new Schema({
     });
   }
 */
-=======
-'use strict';
-
-var mongoose = require('mongoose'),
-Schema = mongoose.Schema;
-
-var IdeaSchema = new Schema({
-  created_at: Date,
-  updated_at: Date,
-    title: {
-      type: String,
-      default: '',
-      trim: true,
-      required: 'Needs a title'
-    },
-    category: {
-      type: String
-    },
-    description: {
-      short: String,
-      long: String
-    },
-    user: {
-      /**
-      * Need to make this an array of user's
-      */
-      type: Schema.ObjectId,
-      ref: 'User'
-    }
-  });
-
-  IdeaSchema.pre('save', function(next) {
-    var currentDate = new Date();
-    this.updated_at = currentDate;
-    if(!this.created_at)
-    this.created_at = currentDate;
-    next();
-  });
-
-  var Idea = mongoose.model('Idea', IdeaSchema);
-  module.exports = Idea;
-
-  /*
-  seeding database to test out the listing view
-
-  var ideas = require('../idealistings.json').ideas;
-  for (var i=0; i < ideas.length; i++) {
-    var item = Idea(ideas[i]);
-    item.save(function(err) {
-      if(err) throw err;
-      console.log("Listing saved!");
-    });
-  }
-*/
->>>>>>> 55dd456882f5558d2e99b5c5df4d1bdeb5862474
--- modules/ideas/server/routes/ideas.server.routes.js
+++ modules/ideas/server/routes/ideas.server.routes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 'use strict';
 
 var ideas = require('../controllers/ideas.server.controller');
@@ -16,22 +16,3 @@ module.exports = function(app) {
 
   app.param('ideaId', ideas.ideaById);
 };
-=======
-'use strict';
-
-var ideas = require('../controllers/ideas.server.controller');
-
-module.exports = function(app) {
-
-  app.route('/api/ideas')
-    .post(ideas.create)
-    .get(ideas.list);
-
-  app.route('/api/ideas/:ideaId')
-    .get(ideas.read)
-    .put(ideas.update)
-    .delete(ideas.delete);
-
-  app.param('ideaId', ideas.ideaById);
-};
->>>>>>> 55dd456882f5558d2e99b5c5df4d1bdeb5862474
