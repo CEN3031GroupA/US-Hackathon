651e635f9fd825735837582bcb666776f03a3207
hackleman <hackleman.jason@gmail.com>
2017-03-29 20:51:37 -0500

    First time screen, hopefully final

--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -21,9 +21,21 @@ angular.module('users').config(['$stateProvider',
         url: '/:userId',
         templateUrl: 'modules/users/client/views/view-user.client.view.html'
       })
+      .state('user.splash', {
+        url: '/welcome',
+        templateUrl: 'modules/users/client/views/welcomescreens/splash.client.view.html'
+      })
       .state('user.welcome', {
         url: '/welcome',
-        templateUrl: 'modules/users/client/views/welcome.client.view.html'
+        templateUrl: 'modules/users/client/views/welcomescreens/welcome.client.view.html'
+      })
+      .state('user.welcome1', {
+        url: '/welcome1',
+        templateUrl: 'modules/users/client/views/welcomescreens/welcome1.client.view.html'
+      })
+      .state('user.welcome2', {
+        url: '/welcome',
+        templateUrl: 'modules/users/client/views/welcomescreens/welcome2.client.view.html'
       });
   }
 ]);
