4f3e8768da09ac9dbe575a2e9dbef040a5bb6fc7
gtbrewster <gtbrewster@ufl.edu>
2017-04-07 14:07:57 -0400

    reformatted code, default js indent etc.

--- modules/users/client/config/users.client.routes.js
+++ modules/users/client/config/users.client.routes.js
@@ -2,24 +2,24 @@
 
 // Setting up route
 angular.module('users').config(['$stateProvider',
-    function ($stateProvider) {
-        // Users state routing
-        $stateProvider
-            .state('user', {
-                abstract: true,
-                url: '/user',
-                template: '<ui-view/>'
-            })
-            .state('signin', {
-                url: '/signin?err',
-                templateUrl: 'modules/users/client/views/signin.client.view.html',
-                data: {
-                    allowAnon: true,
-                }
-            })
-            .state('user.view', {
-                url: '/:userId',
-                templateUrl: 'modules/users/client/views/view-user.client.view.html'
-            });
-    }
+  function ($stateProvider) {
+    // Users state routing
+    $stateProvider
+      .state('user', {
+        abstract: true,
+        url: '/user',
+        template: '<ui-view/>'
+      })
+      .state('signin', {
+        url: '/signin?err',
+        templateUrl: 'modules/users/client/views/signin.client.view.html',
+        data: {
+          allowAnon: true,
+        }
+      })
+      .state('user.view', {
+        url: '/:userId',
+        templateUrl: 'modules/users/client/views/view-user.client.view.html'
+      });
+  }
 ]);
