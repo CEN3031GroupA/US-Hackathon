c9581b8966e76a43f709b2e14b7590f9e222957c
Clinton Andrews <clinton@ignitenet.com>
2017-04-16 21:51:04 -0400

    Add textAngular so we can edit in html

--- modules/subevents/client/subevents.client.module.js
+++ modules/subevents/client/subevents.client.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Use Applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('subevents', ['ui.bootstrap.datetimepicker']);
+ApplicationConfiguration.registerModule('subevents', ['ui.bootstrap.datetimepicker', 'textAngular']);
